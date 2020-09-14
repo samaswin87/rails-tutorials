e5477d005337fd763e99e18240755ec70c846105
samaswin87 <aswin.r312@outlook.com>
2020-09-14 22:50:45 +0530

    fixed the tailwind template for new version

---
+++ vuejs_api/vuejs_front/src/components/Signin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="max-w-sm m-auto my-8">
+    <div class="border p-10 border-gray-400 shadow rounded">
+      <h3 class="text-2xl mb-6 text-gray-800">Sign In</h3>
+      <form @submit.prevent="signin">
+        <div class="text-red" v-if="error">{{ error }}</div>
+
+        <div class="mb-6">
+          <label for="email" class="label">E-mail Address</label>
+          <input type="email" v-model="email" class="input" id="email" placeholder="andy@web-crunch.com">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="label">Password</label>
+          <input type="password" v-model="password" class="input" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-600 block w-full py-4 text-white items-center justify-center">Sign In</button>
+
+        <div class="my-4"><router-link to="/signup" class="link-gray-900">Sign up</router-link></div>
+      </form>
+    </div>
+  </div>
+</template>
