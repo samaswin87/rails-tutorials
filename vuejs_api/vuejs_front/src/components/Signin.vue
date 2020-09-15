d987016c5cd08fed0591e9f58a818910d32fa432
samaswin87 <aswin.r312@outlook.com>
2020-09-15 07:24:50 +0530

    added signin component

--- vuejs_api/vuejs_front/src/components/Signin.vue
+++ vuejs_api/vuejs_front/src/components/Signin.vue
@@ -13,10 +13,64 @@
           <label for="password" class="label">Password</label>
           <input type="password" v-model="password" class="input" id="password" placeholder="Password">
         </div>
-        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-600 block w-full py-4 text-white items-center justify-center">Sign In</button>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green-500 hover:bg-green-600 block w-full py-4 text-white items-center justify-center">Sign In</button>
 
         <div class="my-4"><router-link to="/signup" class="link-gray-900">Sign up</router-link></div>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+
+  created () {
+    this.checkSignedIn()
+  },
+
+  updated () {
+    this.checkSignedIn()
+  },
+
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSussessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+
+    signinSussessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        this.$router.replace('/records')
+      }
+    }
+  }
+
+}
+</script>
