a4919ca2b1205d980405f93bb26f3ce7c690b4d6
samaswin87 <aswin.r312@outlook.com>
2020-09-22 13:45:56 +0530

    added tailwind demo

---
+++ tailwind_demo/postcss.config.js
@@ -0,0 +1,15 @@
+let environment = {
+  plugins: [
+    require('tailwindcss')('./app/javascript/tailwind/tailwind.config.js'),
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+};
+
+module.export = environment;
